daf92b2ad3dbf846f3c206f148083a818bf1749e
Thais Ribeiro <tha.sribeiro@gmail.com>
2018-07-17 21:42:21 -0300

    OnGoing comunicação client

--- PartsProject/src/partsproject/Client.java
+++ PartsProject/src/partsproject/Client.java
@@ -6,40 +6,46 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
 
-
 public class Client implements ClientInterface {
-    
+
     public PartRepositoryInterface servicoRemoto;
     public Part pecaAtual;
-    
-    public static void main(String[] args)  {
-        System.out.printf("começou Cliente");
-        Client cliente  = new Client("alynne",4200);
+
+    public static void main(String[] args) throws RemoteException {
+        
+        Scanner ler = new Scanner(System.in);
+        String serverName;
+        int port;
+        
+        System.out.println("Olá, vamos começar com a nossa conexão!");
+        
+        System.out.println("Primeiro digite o nome do servidor que deseja se conectar");
+        serverName = ler.next();
+        
+        System.out.println("Boa! Agora digite a porta do servidor");
+        port = ler.nextInt();
+        
+        Client cliente = new Client(serverName, port);
+        
+        cliente.showp();
+        
+        
     }
-    
-    
+
     public Client(String serverName, int serverPort) {
         try {
-            
-            System.out.printf("Entrou");
-            
-            String nomeServico = serverName;
-            int porta = serverPort;
 
-           // ClientInterface client = new Client(serverName, serverPort);
-           // ClientInterface clientDistribuido = (ClientInterface) UnicastRemoteObject.exportObject(client, 0);
+            System.out.printf("Estabelecendo conexão...");
+     
+            Registry registry = LocateRegistry.getRegistry(serverPort);
+            this.servicoRemoto = (PartRepositoryInterface) registry.lookup(serverName);
+            //Part part = new Part("teste1", "testeteste", 1, true);
+            //  servicoRemoto.addPart(part); // precisa implementar isso
 
-            Registry registry = LocateRegistry.getRegistry(porta);
-            this.servicoRemoto = (PartRepositoryInterface) registry.lookup(nomeServico);
-            Part part = new Part("teste1","testeteste",1,true);
-          //  servicoRemoto.addPart(part); // precisa implementar isso
-            
-          //Part teste = servicoRemoto.getPart(1);
-           //System.out.printf(teste.getNome());
-             
+            //Part teste = servicoRemoto.getPart(1);
+            //System.out.printf(teste.getNome());
             //servicoRemoto.addListener(clientDistribuido);
-
-            System.out.println("Cliente Online");
+            System.out.println("Cliente Online!");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -47,7 +53,7 @@ public class Client implements ClientInterface {
 
     @Override
     public void bind(String repositoryName) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
@@ -57,23 +63,22 @@ public class Client implements ClientInterface {
 
     @Override
     public Part getp(int id) throws RemoteException {
-        this.pecaAtual = this.servicoRemoto.getPart(id); 
+        this.pecaAtual = this.servicoRemoto.getPart(id);
         return this.pecaAtual;
     }
 
     @Override
     public void showp() throws RemoteException {
-        if(this.pecaAtual != null){
-            System.out.println("Id: "+this.pecaAtual.getId());
-            System.out.println("Nome: "+this.pecaAtual.getNome());
-            System.out.println("Descrição: "+this.pecaAtual.getDescricao());
+        if (this.pecaAtual != null) {
+            System.out.println("Id: " + this.pecaAtual.getId());
+            System.out.println("Nome: " + this.pecaAtual.getNome());
+            System.out.println("Descrição: " + this.pecaAtual.getDescricao());
             Vector<Part> componentes = this.pecaAtual.getComponentes();
             System.out.println("Subcomponentes: ");
-            for(Part p : componentes){
-                System.out.println("Id: "+p.id+", Nome da Peça: "+p.getNome());
+            for (Part p : componentes) {
+                System.out.println("Id: " + p.id + ", Nome da Peça: " + p.getNome());
             }
-        }
-        else{
+        } else {
             System.out.println("Selecione uma peça primeiro!");
         }
     }
@@ -82,15 +87,14 @@ public class Client implements ClientInterface {
     public void clearlist() throws RemoteException {
         this.servicoRemoto.ClearRepository();
     }
-    
+
     @Override
     public void addsubpart(int n) throws RemoteException {
-        if(this.pecaAtual != null){
+        if (this.pecaAtual != null) {
             Part novaPeca = this.pecaAtual;
             novaPeca.quantidade = n;
             this.servicoRemoto.addPart(novaPeca);
-        }
-        else{
+        } else {
             System.out.println("Selecione uma peça primeiro!");
         }
     }
@@ -105,4 +109,5 @@ public class Client implements ClientInterface {
     public void quit(String repositoryName) throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
 }
